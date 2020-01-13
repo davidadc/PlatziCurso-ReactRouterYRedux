8056a588cdb9e62814dd4125936a099701a06a17
David Delgado <dadch13.14@gmail.com>
2020-01-12 20:53:42 -0400

    Crear archivo de rutas

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,7 +12,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
 
   return (
@@ -54,4 +54,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
