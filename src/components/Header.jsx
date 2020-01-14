4115d687f1f3d64e16a527d8071f64da26926bf2
David Delgado <dadch13.14@gmail.com>
2020-01-13 23:46:35 -0400

    Uso de gravatar en nuestro proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,54 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import gravatar from '../utils/gravatar';
 
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/icon-user.png';
 
 import '../assets/styles/components/Header.scss';
 
-const Header = () => (
-  <header className='header'>
-    <Link to='/'>
-      <img className='header__img' src={logo} alt='Platzi Video' />
-    </Link>
+const Header = (props) => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
 
-    <div className='header__menu'>
-      <div className='header__menu--profile'>
-        <img src={userIcon} alt='Ícono de usuario' />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li>
-          <a href='/'>Cuenta</a>
-        </li>
-        <li>
-          <Link to='/login'>Iniciar sesión</Link>
-        </li>
-        {/* <li>
+  return (
+    <header className='header'>
+      <Link to='/'>
+        <img className='header__img' src={logo} alt='Platzi Video' />
+      </Link>
+
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+          {hasUser ? (
+            <img src={gravatar(user.email)} alt={user.email} />
+          ) : (
+            <img src={userIcon} alt='Ícono de usuario' />
+          )}
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <a href='/'>Cuenta</a>
+          </li>
+          <li>
+            <Link to='/login'>Iniciar sesión</Link>
+          </li>
+          {/* <li>
           <a href='/'>Cerrar Sesión</a>
-        </li> */}
-      </ul>
-    </div>
-  </header>
-);
+          </li> */}
+        </ul>
+      </div>
+    </header>
+  );
+};
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+// export default Header;
+export default connect(mapStateToProps, null)(Header);
