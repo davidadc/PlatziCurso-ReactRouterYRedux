30c68e5d3524734cb89882e3f6f50417dcbd7125
David Delgado <dadch13.14@gmail.com>
2020-01-13 22:56:41 -0400

    Agregando validaciones al CarouselItem

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -18,7 +18,7 @@ const Home = ({ myList, trends, originals }) => {
           <Carousel>
             {myList.map((item) => (
               // eslint-disable-next-line react/jsx-props-no-spreading
-              <CarouselItem key={item.id} {...item} />
+              <CarouselItem key={item.id} {...item} isList />
             ))}
           </Carousel>
         </Categories>
