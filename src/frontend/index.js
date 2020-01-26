a0751e58b652922c63fabc6ac8ce41645c2b0c35
David Delgado <dadch13.14@gmail.com>
2020-01-26 19:53:30 -0400

    Hydrate y estado de Redux desde Express

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate } from 'react-dom';
 import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
 import reducer from './reducers';
-import initialState from './initialState';
 
 import App from './routes/App';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, initialState, composeEnhancers());
-const history = createBrowserHistory();
+if (typeof window !== 'undefined') {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const preloadedState = window.__PRELOADED_STATE__;
+  delete window.__PRELOADED_STATE__;
+  const store = createStore(reducer, preloadedState, composeEnhancers());
+  const history = createBrowserHistory();
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <App />
-    </Router>
-  </Provider>,
-  // eslint-disable-next-line comma-dangle
-  document.getElementById('app')
-);
+  hydrate(
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>,
+    // eslint-disable-next-line comma-dangle
+    document.getElementById('app')
+  );
+}
