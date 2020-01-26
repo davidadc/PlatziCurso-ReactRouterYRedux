6b25900a6ae7ecb0fc564bef76eb323e13b153f4
David Delgado <dadch13.14@gmail.com>
2020-01-26 19:24:14 -0400

    Aplicando la función main para renderizado desde el servidor y creación del string de HTML

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
+import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
 import { Provider } from 'react-redux';
 import { createStore, compose } from 'redux';
@@ -10,9 +10,7 @@ import initialState from './initialState';
 import App from './routes/App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = createStore(reducer, initialState, composeEnhancers());
-
 const history = createBrowserHistory();
 
 ReactDOM.render(
