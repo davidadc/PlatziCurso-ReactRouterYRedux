9df0ff0e027816a372315087b74a0493f6cdd586
David Delgado <dadch13.14@gmail.com>
2020-05-24 13:01:01 -0400

    Servir React con Express

--- src/server/server.js
+++ src/server/server.js
@@ -23,7 +23,22 @@ if (ENV === 'development') {
 }
 
 app.get('*', (req, res) => {
-  res.send({ hello: 'express' });
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>Platzi Video</title>
+      <link rel="stylesheet" type="text/css" href="assets/app.css">
+    </head>
+    <body>
+      <div id="app"></div>
+      <script src="assets/app.js" type="text/javascript"></script>
+    </body>
+  </html>
+  `);
 });
 
 app.listen(PORT, (error) => {
